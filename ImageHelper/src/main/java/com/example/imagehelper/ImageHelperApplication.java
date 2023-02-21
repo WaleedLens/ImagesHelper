64f660ce46c29ed58a05d6a4650ebf685b327cbb
Waleed Qoozi <90866317+WaleedLens@users.noreply.github.com>
2023-02-21 20:58:14 +0300

    Init commit

---
+++ ImageHelper/src/main/java/com/example/imagehelper/ImageHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageHelperApplication.class, args);
+    }
+
+}
---
+++ ImageHelper/src/test/java/com/example/imagehelper/ImageHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
